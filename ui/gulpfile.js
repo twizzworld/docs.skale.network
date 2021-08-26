fa2b625862ef30759cafdff6bccf1b941b4792c7
Chadwick Strange <cstrangedk@users.noreply.github.com>
2021-08-25 19:52:51 -0700

    Add netlify search

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -17,7 +17,7 @@ const serverConfig = { host: '0.0.0.0', port: 5252, livereload }
 const task = require('./gulp.d/tasks')
 const glob = {
   all: [srcDir, previewSrcDir],
-  css: [`${srcDir}/css/**/*.css`, `!${srcDir}/css/**/*.min.css`, `!${srcDir}/css/site.css`],
+  css: [`${srcDir}/css/**/*.css`, `!${srcDir}/css/**/*.min.css`, `!${srcDir}/css/site*.css`],
   js: ['gulpfile.js', 'gulp.d/**/*.js', `${srcDir}/{helpers,js}/**/*.js`, `!${srcDir}/js/**/*.min.js`],
 }
 
